bc5cfe324b9f7a641d4ea34b7e7eea250595277d
Edu Caro <eduardo.caro.lorente@gmail.com>
2024-01-15 09:30:55 +0100

    Ejercicio terminado

--- Main.java
+++ Main.java
@@ -2,6 +2,26 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-
+        String resultado;
+        String nombre = Leer.leerTexto("Introduce tu nombre: ");
+        String apellidos = Leer.leerTexto("Introduce tus apellidos: ");
+        int edad = Leer.leerEntero("Introduce tu edad: ");
+        double peso = Leer.leerDouble("Introduce tu peso actual: ");
+        double altura = Leer.leerDouble("Introduce tu altura: ");
+        String telefono = Leer.leerTexto("Introduce tu número de teléfono: ");
+        double valor =  peso/(Math.pow(altura,2));
+        if (valor< 18.5){
+            resultado = "Bajo";
+        } else if(valor < 24.9){
+            resultado = "Normal";
+        }else if(valor <29.9){
+            resultado = "Sobrepeso";
+        }else{
+            resultado = "Obesidad";
+        }
+        System.out.println(nombre+" "+apellidos+" de "+edad+" años, los datos introducidos son correctos");
+        System.out.println("El teléfono de contacto es "+telefono);
+        System.out.println("Se ha registrado su altura ("+altura+") y su peso ("+peso+")");
+        System.out.println("Su IMC es: "+valor+". Nivel: "+resultado);
     }
 }
